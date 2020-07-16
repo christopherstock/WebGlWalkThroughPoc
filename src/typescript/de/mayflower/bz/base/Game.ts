f0cc2e2493f4df139111d10a0a538f1fe2036d84
Christopher Stock <christopher.stock@mayflower.de>
2020-07-16 22:12:34 +0200

    Add basic PhotoDome.

--- src/typescript/de/mayflower/bz/base/Game.ts
+++ src/typescript/de/mayflower/bz/base/Game.ts
@@ -12,68 +12,93 @@
         ***************************************************************************************************************/
         public start() : void
         {
-            bz.Main.log( 'Init app' );
+            bz.Main.log('Init app');
 
-            const canvas        = document.createElement( 'canvas' );
-            const canvasContext = canvas.getContext( 'webgl' );
-            canvas.width  = window.innerWidth;
+            const canvas = document.createElement('canvas');
+            const canvasContext = canvas.getContext('webgl');
+            canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-            document.body.appendChild( canvas );
+            document.body.appendChild(canvas);
 
-            const engine = new BABYLON.Engine( canvasContext, true );
-            const scene  = new BABYLON.Scene(engine);
-            const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+            const engine = new BABYLON.Engine(canvasContext, true);
+            const scene = new BABYLON.Scene(engine);
 
+            //const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
 
+            /*
+                // This targets the camera to scene origin
+                camera.setTarget(BABYLON.Vector3.Zero());
 
+                // This attaches the camera to the canvas
+                camera.attachControl(canvas, true);
 
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+                // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+                const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+                // Default intensity is 1. Let's dim the light a small amount
+                light.intensity = 0.7;
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+                // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+                // let sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+                // // Move the sphere upward 1/2 its height
+                // sphere.position.y = 1;
+                // sphere.scaling.scaleInPlace(100)
 
-    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-    // let sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
+                // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+                const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+                const dome = new BABYLON.PhotoDome(
+                    'testdome',
+                    'res/image/skybox/market360.jpg',
+                    {
+                        resolution: 32,
+                        size: 1000,
+                    },
+                    scene
+                );
+                // We can't use dome.mesh.visibility, so we pick up the dome.mesh into a let sphere so we can use visibility
+                 const sphere = dome.mesh;
+                 const mat = new BABYLON.StandardMaterial('', scene)
+                 mat.disableLighting = true
+                 mat.emissiveTexture = new BABYLON.Texture('res/image/skybox/market360.jpg', scene, undefined, false)
 
-    // // Move the sphere upward 1/2 its height
-    // sphere.position.y = 1;
-    // sphere.scaling.scaleInPlace(100)
+                 // mat.sideOrientation = 0
+                 sphere.material = mat
 
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-    const dome = new BABYLON.PhotoDome(
-        'testdome',
-        'res/image/skybox/market360.jpg',
-        {
-            resolution: 32,
-            size: 1000,
-        },
-        scene
-    );
-    // We can't use dome.mesh.visibility, so we pick up the dome.mesh into a let sphere so we can use visibility
-     const sphere = dome.mesh;
-     const mat = new BABYLON.StandardMaterial('', scene)
-     mat.disableLighting = true
-     mat.emissiveTexture = new BABYLON.Texture('res/image/skybox/market360.jpg', scene, undefined, false)
+                scene.onBeforeRenderObservable.add(()=>{
+                    sphere.visibility -= 0.01
+                })
 
-     // mat.sideOrientation = 0
-     sphere.material = mat
+                bz.Main.log( 'Completed init !' );
+            */
 
-    scene.onBeforeRenderObservable.add(()=>{
-        sphere.visibility -= 0.01
-    })
+            const camera2 = new BABYLON.ArcRotateCamera('Camera', -Math.PI / 2, Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
+            camera2.attachControl(canvas, true);
+            camera2.inputs.attached.mousewheel.detachControl(canvas);
 
-    bz.Main.log( 'Completed init !' );
+            const dome = new BABYLON.PhotoDome(
+                'testdome',
+                'res/image/skybox/market360.jpg',
+                {
+                    resolution: 32,
+                    size: 1000,
+                },
+                scene
+            );
 
+            // Register a render loop to repeatedly render the scene
+            engine.runRenderLoop(function () {
+                scene.render();
+            });
 
+            // Watch for browser/canvas resize events
+            window.addEventListener('resize', function () {
 
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+
+                engine.resize();
+            });
 
         }
     }
